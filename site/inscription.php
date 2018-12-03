ee736237370694d8d2a388a95804c25464c5c125
sanzmata1834 <sanzmata1834@he-ferrer.eu>
2018-12-03 06:02:33 +0100

    Modifs PHP inscription

--- site/inscription.php
+++ site/inscription.php
@@ -5,9 +5,16 @@ $date = $_POST["datepicker"];
 $cp = $_POST["cp"];
 $ville = $_POST["ville"];
 $mail = $_POST["mail"];
-$mdp1 = $_POST["mdp1"];
+$mdp1 = $_POST["mdp"];
 $mdp2 = $_POST["mdp2"];
 
-echo "Vous êtes bien enregistrés, voici vos données :<br>";
-echo $nom, $prenom, $date, $cp, $ville, $mail, $mdp1, $mdp2;
+echo "<h1>Formulaire bien envoyé, voici vos données :</h1>";
+echo "<p>Votre nom : ".$nom."</p>";
+echo "<p>Votre prenom : ".$prenom."</p>";
+echo "<p>Votre date de naissance : ".$date."</p>";
+echo "<p>Votre code postal : ".$cp."</p>";
+echo "<p>Votre ville : ".$ville."</p>";
+echo "<p>Votre e-mail : ".$email."</p>";
+echo "<p>Votre MDP : ".$mdp."</p>";
+echo "<p>Votre confirmation de MDP : ".$mdp2."</p>";
 ?>
