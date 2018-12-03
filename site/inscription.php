719abbdafc8bba22b68c3f35f9b303127d929bc9
sanzmata1834 <sanzmata1834@he-ferrer.eu>
2018-12-03 06:07:56 +0100

    Correction PHP ajout de bold

--- site/inscription.php
+++ site/inscription.php
@@ -9,12 +9,12 @@ $mdp = $_POST["mdp"];
 $mdp2 = $_POST["mdp2"];
 
 echo "<h1>Formulaire bien envoyé, voici vos données :</h1>";
-echo "<p>Votre nom : ".$nom."</p>";
-echo "<p>Votre prenom : ".$prenom."</p>";
-echo "<p>Votre date de naissance : ".$date."</p>";
-echo "<p>Votre code postal : ".$cp."</p>";
-echo "<p>Votre ville : ".$ville."</p>";
-echo "<p>Votre e-mail : ".$email."</p>";
-echo "<p>Votre MDP : ".$mdp."</p>";
-echo "<p>Votre confirmation de MDP : ".$mdp2."</p>";
+echo "<p>Votre nom : <b>".$nom."</b></p>";
+echo "<p>Votre prenom : <b>".$prenom."</b></p>";
+echo "<p>Votre date de naissance : <b>".$date."</b></p>";
+echo "<p>Votre code postal : <b>".$cp."</b></p>";
+echo "<p>Votre ville : <b>".$ville."</b></p>";
+echo "<p>Votre e-mail : <b>".$email."</b></p>";
+echo "<p>Votre MDP : <b>".$mdp."</b></p>";
+echo "<p>Votre confirmation de MDP : <b>".$mdp2."</b></p>";
 ?>
